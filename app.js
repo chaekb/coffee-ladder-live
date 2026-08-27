f38610f013aff81f94b1c5d0185df092426d6a23
chaekb <chaekyoungbyoung@gmail.com>
2026-08-27 14:43:15 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
+function createRoom(){
 
-console.log("Coffee Ladder Start");
+    const room =
+    Math.floor(100000 + Math.random()*900000);
+
+
+    location.href =
+    "room.html?room=" + room;
+
+}
+
+
+function joinRoom(){
+
+    const room =
+    prompt("방 번호 입력");
+
+
+    if(room){
+
+        location.href =
+        "room.html?room=" + room;
+
+    }
+
+}
