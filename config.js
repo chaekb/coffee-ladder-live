8653bcfd7dc4f4a6990c2ff7f6f86c0c4913f7eb
chaekb <chaekyoungbyoung@gmail.com>
2026-08-28 14:27:21 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,11 @@
 const firebaseConfig = {
   apiKey: "AIzaSyC7wrf_nfAoGtXX8dWQxkMzArXU2jDnmoc",
   authDomain: "coffee-ladder-live.firebaseapp.com",
+  databaseURL: "https://coffee-ladder-live-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "coffee-ladder-live",
-  databaseURL: "https://coffee-ladder-live-default-rtdb.asia-southeast1.firebasedatabase.app"
+  storageBucket: "coffee-ladder-live.firebasestorage.app",
+  messagingSenderId: "112031075471",
+  appId: "1:112031075471:web:6744be54ad11a3d6ad74fa"
 };
 
 // Firebase 초기화 및 모듈 내보내기용 전역 변수 (www 고정 주소로 수정 완료)
